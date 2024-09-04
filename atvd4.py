a130f7b47c258c7536bc3bfd9a900f75a5c4564f
mairoklpss <mairalopesma@gmail.com>
2024-09-04 15:34:13 -0300

    calcular média de 2 números

---
+++ atvd4.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+n1 = int(input("nota 1: "))
+n2 = int(input("nota 2: "))
+media = (n1 + n2)/2
+
+print(f"a média do aluno é: {media}")
